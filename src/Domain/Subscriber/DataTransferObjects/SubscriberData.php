3097630864474e8885be241b9c634ded1635061b
HassanElshazlyEida <hassanaboeata@gmail.com>
2024-09-05 15:02:39 +0300

    💥 NEW: Factories & DTO & Action

---
+++ src/Domain/Subscriber/DataTransferObjects/SubscriberData.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Domain\Subscriber\DataTransferObjects;
+
+use Illuminate\Http\Request;
+use Spatie\LaravelData\Data;
+use Illuminate\Validation\Rule;
+use Domain\Subscriber\Models\Tag;
+use Domain\Subscriber\Models\Form;
+use Spatie\LaravelData\DataCollection;
+
+class SubscriberData extends Data
+{
+    public function __construct(
+        public readonly ?int            $id,
+        public readonly string          $email,
+        public readonly string          $first_name,
+        public readonly ?string         $last_name,
+        /** @var DataCollection<TagData> */
+        public readonly ?DataCollection $tags,
+        public readonly ?FormData       $form,
+    )
+    {
+    }
+
+    public static function fromRequest(Request $request): self
+    {
+
+        return self::from([
+            ...$request->all(),
+            'tags' => TagData::collection(Tag::whereIn('id', $request->collect('tag_ids'))->get()),
+            'form' => FormData::from(Form::findOrNew($request->form_id)),
+        ]);
+    }
+
+    public static function rules(): array
+    {
+        return [
+            'email' => [
+                'required',
+                'email',
+                Rule::unique('subscribers', 'email')->ignore(request('subscriber')),
+            ],
+            'first_name' => ['required', 'string'],
+            'last_name' => ['nullable', 'sometimes', 'string'],
+            'tags' => ['nullable', 'sometimes', 'array'],
+            'form_id' => ['nullable', 'sometimes', 'exists:forms,id'],
+        ];
+    }
+}
--- src/Domain/Subscriber/Models/Subscriber.php
+++ src/Domain/Subscriber/Models/Subscriber.php
@@ -15,6 +15,14 @@ class Subscriber extends BaseModel
         'form_id',
         'user_id',
     ];
-  
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
+    public function form(): BelongsTo
+    {
+        return $this->belongsTo(Form::class)->withDefault();
+    }
 
 }
